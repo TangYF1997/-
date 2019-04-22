99ed38939e477ca49faeae0173925a8135b08668
TangYF1997 <36926231+TangYF1997@users.noreply.github.com>
2019-04-22 11:01:21 +0800

    大窗打开小窗

--- GUI_learing/main.py
+++ GUI_learing/main.py
@@ -1,28 +1,40 @@
 import sys
-import ceshi
-import baidu_faceset
+import faceset_ui
+import w1
 from PyQt5 import QtWidgets, QtGui
 
 
-class Mywindow(QtWidgets.QMainWindow,ceshi.Ui_MainWindow):
-    def __init__(self, parent = None):
+class Mywindow(QtWidgets.QMainWindow, faceset_ui.Ui_MainWindow):
+    def __init__(self, parent=None):
         super(Mywindow, self).__init__()
 
         QtWidgets.QMainWindow.__init__(self, parent)
         self.setupUi(self)
 
-    def pl_ck(self):
-        name = self.lineEdit.text()
+    def show_w1(self):#显示窗体2
+        w1.show()
 
-        age = self.lineEdit_2.text()
-        age = int(age)
-        c = baidu_faceset.get_info(name, age)
-        self.textBrowser.append(c)
+
+class Dialog1(QtWidgets.QMainWindow, w1.Ui_Dialog):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    def accept(self):
+        self.accept()
+
+    def reject(self):
+
+        self.close()
+
+# 有错误不报才是最气的，草
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = Mywindow()
-    # window.resize(700, 600)
+    w1 = Dialog1()
     window.show()
+    # window.resize(700, 600)
+    # w1.show()
     sys.exit(app.exec_())
